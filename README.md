d03c7483c8c5d8b428ac20b0184ead573a28b38e
Ayrton Felipe <ayrton.tunes@outlook.com>
2018-01-15 21:25:15 -0200

    Initial commit   - Writing basic documentation.   - Defining initial library dependences.   - Create initial spec file.

---
+++ README.md
@@ -0,0 +1,3 @@
+# spotfy-wrapper
+An simple spotfy wrapper builded with JavaScript ES6 and TDD methodology.
+
---
+++ specs/main.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+/**
+ * Spotfy Wrapper
+ * This is an simple exercise to put in praticle some concepts learned in Js TDD course.
+ * */
+
+describe("Spotfy Wrapper", function() {
+  
+});
